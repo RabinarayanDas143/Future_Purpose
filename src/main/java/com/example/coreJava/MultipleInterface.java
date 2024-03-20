a73071e5f6c86c05c1682c57156532efa46154b0
Rabi <dasrabinarayn@gmail.com>
2024-03-20 18:24:47 +0530

    kolia

--- src/main/java/com/example/coreJava/MultipleInterface.java
+++ src/main/java/com/example/coreJava/MultipleInterface.java
@@ -1,34 +1,41 @@
 package com.example.coreJava;
+
 @FunctionalInterface
-interface printable{
+interface printable {
 	public void print();
-	public static String method1(){
+
+	public static String method1() {
 		return "Rabii";
 	}
+	
+	default void test1() {
+		System.out.println("default method of printable");
+	}
 }
+
 @FunctionalInterface
-interface display{
+interface display {
 	public void print();
-	public static String method1(){
+
+	public static String method1() {
 		return "Aakash";
 	}
-};
-public class MultipleInterface implements printable,display{
-
-	 
 	
+//	default void test1() {
+//		System.out.println("default method of display");
+//	}
+};
+
+public class MultipleInterface implements printable, display {
+
 	public static void main(String[] args) {
 		MultipleInterface obj = new MultipleInterface();
-		 
+		display.method1();
 	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	display.super.method1();
- 
 
+	}
+ 
 }
