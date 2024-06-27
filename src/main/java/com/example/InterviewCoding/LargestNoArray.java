ae0e3d17953853e4847771c8a5d459158d46e829
Rabi <dasrabinarayn@gmail.com>
2024-06-27 23:42:14 +0530

    27/06/24

--- src/main/java/com/example/InterviewCoding/LargestNoArray.java
+++ src/main/java/com/example/InterviewCoding/LargestNoArray.java
@@ -1,19 +1,19 @@
 package com.example.InterviewCoding;
 
 public class LargestNoArray {
-public static void main(String[] args) {
-	int[] intArray= {2,4,6,8,9,10,55};
-	int min=intArray[0];
-	int max=intArray[0];
-	
-	for(int i=0;i<intArray.length;i++) {
-		if(intArray[i]>max) {
-			max=intArray[i];
-		}else if(intArray[i]<min) {
-			min=intArray[i];
+	public static void main(String[] args) {
+		int[] intArray = { 2, 4, 6, 8, 9, 10, 55 };
+		int min = intArray[0];
+		int max = intArray[0];
+
+		for (int i = 0; i < intArray.length; i++) {
+			if (intArray[i] > max) {
+				max = intArray[i];
+			} else if (intArray[i] < min) {
+				min = intArray[i];
+			}
 		}
+		System.out.println("grater no is " + max);
+		System.out.println("smaller no is " + min);
 	}
-	System.out.println("grater no is "+max);
-	System.out.println("smaller no is "+min);
-}
 }
--- src/main/java/com/example/InterviewCoding/ReverseString.java
+++ src/main/java/com/example/InterviewCoding/ReverseString.java
@@ -11,9 +11,16 @@ public class ReverseString {
 	}
 
 	public static void main(String[] ram) {
-		ReverseString obj = new ReverseString();
+		//ReverseString obj = new ReverseString();
 		//String result = obj.doReverse("Rabinarayan Das");
 		//System.out.println(doReverse("RAbinarayan Das"));
 		//System.out.println(result);
+		String str ="IDBI";
+		StringBuffer sbr = new StringBuffer();
+		for (int i = str.length() - 1; i >= 0; i--) {
+			sbr = sbr.append(str.charAt(i));
+		}
+		String result = sbr.toString();
+		System.out.println(result);
 	}
 }
